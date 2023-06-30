cb2096c7717bd201f90d9c2d82cd10fd0ba838c4
Rishabh Parsediya <codingchannelrp@gmail.com>
2023-06-30 21:07:04 +0530

    initial commit for part5

--- part5/index.js
+++ part5/index.js
@@ -1,78 +1,30 @@
-const input = document.querySelector("#input");
-input.addEventListener("input", updateValue);
-
-const hideButton = document.querySelector(".search__button");
-const fabutton = document.querySelector("#fa");
-let hide = true;
-
-hideButton.addEventListener("click", () => {
-  hide = !hide;
-
-  if (fabutton.classList.contains("fa-eye")) {
-    fabutton.classList.remove("fa-eye");
-    fabutton.classList.add("fa-eye-slash");
-    input.type = "text";
-  } else {
-    fabutton.classList.remove("fa-eye-slash");
-    fabutton.classList.add("fa-eye");
-    input.type = "password";
+function example1() {
+  var x = 10;
+  if (true) {
+    var x = 20;
+    console.log(x); // 20
   }
-});
-
-function updateValue(e) {
-  let password = e.target.value;
-
-  if (hasCapitalLetter(password)) {
-    document.querySelector("#li1").classList.add("success");
-  } else {
-    document.querySelector("#li1").classList.remove("success");
-  }
-  if (hasSmallLetter(password)) {
-    document.querySelector("#li2").classList.add("success");
-  } else {
-    document.querySelector("#li2").classList.remove("success");
-  }
-
-  if (hasNumber(password)) {
-    document.querySelector("#li3").classList.add("success");
-  } else {
-    document.querySelector("#li3").classList.remove("success");
-  }
-
-  if (hasSymbol(password)) {
-    document.querySelector("#li4").classList.add("success");
-  } else {
-    document.querySelector("#li4").classList.remove("success");
-  }
-
-  if (hasMinimumLength(password, 8)) {
-    document.querySelector("#li5").classList.add("success");
-  } else {
-    document.querySelector("#li5").classList.remove("success");
-  }
+  console.log(x); // 20
 }
 
-function hasCapitalLetter(password) {
-  const pattern = /[A-Z]/;
-  return pattern.test(password);
-}
+example1()
 
-function hasSmallLetter(password) {
-  const pattern = /[a-z]/;
-  return pattern.test(password);
+function example2() {
+  let x = 10;
+  if (true) {
+    let x = 20;
+    console.log(x); // 20
+  }
+  console.log(x); // 10
 }
+example2()
 
-function hasMinimumLength(password, minLength) {
-  return password.length >= minLength;
+function example() {
+  const x = 10;
+  if (true) {
+    const x = 20;
+    console.log(x); // 20
+  }
+  console.log(x); // 10
 }
-
-function hasNumber(password) {
-  const pattern = /[0-9]/;
-  return pattern.test(password);
-}
-
-// Example usage
-function hasSymbol(password) {
-  const pattern = /[!@#$%^&*()\-_=+{};:,<.>]/;
-  return pattern.test(password);
-}
+example3()
